3154e89d6e59145220d9d0296cc65b4f13380494
Sóstenes Melo <sostenesilvaa@gmail.com>
2024-09-01 16:17:50 -0300

    retirar + static no urls

--- portaltlsolucoes/urls.py
+++ portaltlsolucoes/urls.py
@@ -6,4 +6,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('portaltl.urls')),
-]
+] + static(settings.STATIC_URL, document_root = settings.STATICFILES_DIRS[0]) + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
